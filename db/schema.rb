2f5a76804d7947eaa42e27a5afd6db40bdd55cd8
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2024-05-31 17:00:17 +0200

    Links resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_05_31_143610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "links", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
