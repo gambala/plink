7d153c62f79c01b9eb94ce71692cdcd51da72f88
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2024-06-06 17:44:13 +0200

    Create Page resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_05_31_143610) do
+ActiveRecord::Schema[8.0].define(version: 2024_06_06_153823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "links", force: :cascade do |t|
     t.string "title"
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
